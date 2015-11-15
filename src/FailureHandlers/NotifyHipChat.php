234c2837f7951017f6f285875c3c35ec9e7286cf
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2015-11-15 00:35:37 +0000

    ensure from is no longer than 15 chars for hip chat notification

--- src/FailureHandlers/NotifyHipChat.php
+++ src/FailureHandlers/NotifyHipChat.php
@@ -13,6 +13,8 @@ class NotifyHipChat implements OnFailureInterface {
             $from = $config['hipchat']['from'];
         }
 
+        $from = substr($from, 0, 15);
+
         $hc = new HipChat($config['hipchat']['api_key']);
         $hc->message_room($config['hipchat']['room'], $from, '<b>[host:' . gethostname() . ']</b> - ' . $config['hipchat']['message'], true, HipChat::COLOR_RED);
         $hc->message_room($config['hipchat']['room'], $from, json_encode(json_decode($lastSuccessStatus), JSON_PRETTY_PRINT), false, HipChat::COLOR_RED, HipChat::FORMAT_TEXT);
